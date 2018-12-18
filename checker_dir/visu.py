2df6f96ea591354f1fbab8d404c2aedd2f9fd198
BeJF <33451439+BeJF@users.noreply.github.com>
2018-12-18 09:24:29 +0100

    welcome page for the visu

--- checker_dir/visu.py
+++ checker_dir/visu.py
@@ -13,28 +13,28 @@ mod = 2
 speed = 0
 start = 0
 
-pile = str(random.sample(range(1, 10), 5))
-pile = (pile.replace(",", "")
-            .replace("[", "")
-            .replace("]", ""))
-print(pile)
+#pile = str(random.sample(range(1, 10), 5))
+#pile = (pile.replace(",", "")
+ #           .replace("[", "")
+  #          .replace("]", ""))
+#print(pile)
 
 # Get initial data
 # We read the file line by line and store them inside a list.
 # We take the first line (the initial pile), to know the number of datas we will have to sort
 
-cmd = './push_swap %s | ./checker %s' % (pile, pile)
-os.system(cmd)
+#cmd = './push_swap %s | ./checker %s' % (pile, pile)
+#os.system(cmd)
 
-lines = [line.rstrip('\n') for line in open('piles')]
-initial_pile = list(map(int,lines[0].split())) 
-size = len(initial_pile)
+#lines = [line.rstrip('\n') for line in open('piles')]
+#initial_pile = list(map(int,lines[0].split())) 
+#size = len(initial_pile)
 
 # Set other variable for the size of the datas
-max = max(initial_pile)
-data_height = (win_height - 200) / size
-data_width_ratio = (win_width - 200) / max / 2
-max_x1_a = data_width_ratio * max
+#max = max(initial_pile)
+#data_height = (win_height - 200) / size
+#data_width_ratio = (win_width - 200) / max / 2
+#max_x1_a = data_width_ratio * max
 
 
 def create_piles(nb_datas):
@@ -115,7 +115,7 @@ def Start() :
     elif start == 2 :
         start = 0
         Pause.set("Start")
-        display()
+        display(lines)
 
 def update_piles(pile, i) :
     
@@ -128,7 +128,7 @@ def update_piles(pile, i) :
     if speed < 0 :
         time.sleep((-speed)/50)
 
-def display() :
+def display(lines) :
     
     i = 1
     global start
@@ -152,21 +152,68 @@ def display() :
     start = 2
     Pause.set("Reset")
 
+def Set_variables(lines):
+    global size
+    global data_height
+    global data_width_ratio
+    global max_x1_a
+    global max
+
+    initial_pile = list(map(int,lines[0].split())) 
+    max = max(initial_pile)
+    size = len(initial_pile)
+    data_height = (win_height - 200) / size
+    data_width_ratio = (win_width - 200) / max / 2
+    max_x1_a = data_width_ratio * max
+
+def Set_buttons() :
+    
+    # Set the "Start/Stop/Reset button"
+    global Pause
+    Pause = StringVar()
+    Pause.set("Start")
+    go = Button(master, width=20, textvariable = Pause, highlightbackground="black", command = Start) #command = start
+    go.place(x=500, y=25)
+
+    # Set the "Speed up" and "Slow down" buttons
+    slow_down_button = Button(master, width=10, text = "Speed Up", highlightbackground="black", command = Speed_up)
+    slow_down_button.place(x=485, y=50)
+    speed_up_button = Button(master, width=10, text = "Slow Down", highlightbackground="black", command = Slow_down)
+    speed_up_button.place(x=605, y=50)
 
 def Set_nb_datas() :
+    global max
+    global lines
     nb_datas = int(E1.get())
     range_data = int(E2.get())
     pile = str(random.sample(range(1, 1000), nb_datas))
     pile = (pile.replace(",", "")
             .replace("[", "")
             .replace("]", ""))
+    cmd = './push_swap %s | ./checker %s' % (pile, pile)
     L1.destroy()
     E1.destroy()
     L2.destroy()
     E2.destroy()
+    L3.destroy()
+    L4.destroy()
+    L5.destroy()
     ok_button.destroy()
-    create_piles(size)
-    display()
+    os.system(cmd)
+    
+    lines = [line.rstrip('\n') for line in open('piles')]
+    Set_variables(lines)
+    Set_buttons()
+    '''initial_pile = list(map(int,lines[0].split())) 
+    max = max(initial_pile)
+    size = len(initial_pile)
+    data_height = (win_height - 200) / size
+    data_width_ratio = (win_width - 200) / max / 2
+    max_x1_a = data_width_ratio * max'''
+    
+    create_piles(nb_datas)
+    #create_piles(size)
+    display(lines)
     print(nb_datas)
 
 # Set the window
@@ -174,25 +221,32 @@ master = Tk()
 title = "VISU PUSH_SWAP     \u00A9jfinet"
 master.title(title)
 w = Canvas(master, height=win_height, width=win_width, bg='black')
-#test = Canvas(master, height=100, width=100, bg='black')
-#test.place(y=0,x=0)
 w.pack()
 
-L1 = Label(master, text="Number of datas")
-E1 = Entry(master) #bd=5
-L2 = Label(master, text="Range of data")
-E2 = Entry(master)
-
-ok_button = Button(master,text="Ok", command=Set_nb_datas)
-
+L1 = Label(master, bg="black", fg="white", text="Number of datas")
 L1.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+E1 = Entry(master)
 E1.place(relx= 0.5, rely= 0.53, anchor=CENTER)
+
+L2 = Label(master, bg="black", fg="white", text="Range")
 L2.place(relx=0.5, rely=0.57, anchor=CENTER)
+
+E2 = Entry(master)
 E2.place(relx= 0.5, rely= 0.60, anchor=CENTER)
+
+L3 = Label(master, bg="black", fg="white", text="- 19 -", font="Verdana 80 bold")
+L3.place(relx=0.5, rely=0.1, anchor=CENTER)
+L4 = Label(master, bg="black", fg="white", text="The Best Coding School - Asgard", font="Verdana 30 bold")
+L4.place(relx=0.5, rely=0.2, anchor=CENTER)
+L5 = Label(master, bg="black", fg="white", text="The program will generate a random unsorted pile\n with the number of datas you entered (< 1000),\n within the range you decided (< number datas),\n and will sort it.", font="Verdana 14")
+L5.place(relx=0.5, rely=0.35, anchor=CENTER)
+
+ok_button = Button(master,text="Ok", width=5, highlightbackground="black", command=Set_nb_datas)
 ok_button.place(relx=0.5, rely=0.65, anchor=CENTER)
 
 
-# Set the "Start/Stop/Reset button"
+'''# Set the "Start/Stop/Reset button"
 Pause = StringVar()
 Pause.set("Start")
 go = Button(master, width=20, textvariable = Pause, highlightbackground="black", command = Start) #command = start
@@ -202,7 +256,7 @@ go.place(x=500, y=25)
 slow_down_button = Button(master, width=10, text = "Speed Up", highlightbackground="black", command = Speed_up)
 slow_down_button.place(x=485, y=50)
 speed_up_button = Button(master, width=10, text = "Slow Down", highlightbackground="black", command = Slow_down)
-speed_up_button.place(x=605, y=50)
+speed_up_button.place(x=605, y=50)'''
 
 # Launch the fcking algo to display the piles being sorted
 #create_piles(size)
